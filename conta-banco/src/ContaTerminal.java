7f4dcac2db6911692043f8f520e5deb22ab4b05c
Rosana <rosana_svieira@hotmail.com>
2023-09-10 17:02:33 -0300

    projeto finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,8 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello, World!");
+        Scanner Scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println(" Por favor, digite o número da Conta ");
+        int contaCliente = Scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência ");
+        String agenciaCliente = Scanner.next();
+
+        System.out.println("Por favor, digite o seu primeiro nome: ");
+        String primeiroNomeCliente = Scanner.next();
+
+        System.out.println("Por favor, digite o seu sobrenome: ");
+        String sobrenomeCliente = Scanner.next();;
+
+	    double saldoCliente = 237.48;
         
+
+	    System.out.println("Olá " + primeiroNomeCliente + " " + sobrenomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + agenciaCliente + " conta " + contaCliente + " e seu saldo é R$ " + saldoCliente + " já está disponível para saque.");
+
     }
 }
